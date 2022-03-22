895ddc4227d2f7fcdda761f7d9e384fdbd11e9c0
Agnes <koinageagness@gmail.com>
2022-03-22 13:33:20 +0300

    add galleries to gallary section

--- scripts/script.js
+++ scripts/script.js
@@ -16,36 +16,4 @@ document.getElementById('contact-form').submit();
 
 
 
-    
-
-
-
-
-
-// function validateForm() {
-//     var name =  document.getElementById('name').value;
-//     if (name == "") {
-//         document.querySelector('.status').innerHTML = "Name cannot be empty";
-//         return false;
-//     }
-//     var email =  document.getElementById('email').value;
-//     if (email == "") {
-//         document.querySelector('.status').innerHTML = "Email cannot be empty";
-//         return false;
-//     } 
-//     var subject =  document.getElementById('subject').value;
-//     if (subject == "") {
-//         document.querySelector('.status').innerHTML = "Subject cannot be empty";
-//         return false;
-//     }
-//     var message =  document.getElementById('message').value;
-//     if (message == "") {
-//         document.querySelector('.status').innerHTML = "Message cannot be empty";
-//         return false;
-//     }
-//     document.querySelector('.status').innerHTML = "Sending...";
-//   }
-
-
-
 
