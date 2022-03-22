80de277dd4f89e9e0917370fec1c4c8dbc82d09a
iano <ianmwema07@gmail.com>
2022-03-22 13:15:27 +0300

    add code to js file

--- scripts/script.js
+++ scripts/script.js
@@ -1,51 +1,24 @@
+function Appointment(firstName,lastName,service,style,date,time)
+{
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.service = service;
+    this.style = style;
+    this.date = date;
+    this.time = time;
+}
 
-document.getElementById('contact-form').submit();
-    function messageValidate () {
-        var customerName = document.getElementById("name").value;
-        var customerEmail= document.getElementById("email").value;
-        var customerMessage = document.getElementById("message").value;
-        if (customerName == "" || customerEmail  == ""|| customerMessage  == ""){
-        alert("Please enter your name, email and message.");
-        return false;
+$(document).ready(function(){
+    $("form#appointmentForm").submit(function(event){
+        event.preventDefault();
+        var firstName = $("input#firstName").val();
+        var lastName = $("input#lastName").val();
+        var service = $("input#service").val();
+        var style = $("input#style").val();
+        var date = $("input#date").val();
+        var time = $("input#time").val();
 
-        }
-        else{
-        alert ("Your message have been received we will get back to you!");
-            }
-        }
+        var newAppointment = new Appointment(firstName,lastName,service,style,date,time);
 
-
-
-    
-
-
-
-
-
-// function validateForm() {
-//     var name =  document.getElementById('name').value;
-//     if (name == "") {
-//         document.querySelector('.status').innerHTML = "Name cannot be empty";
-//         return false;
-//     }
-//     var email =  document.getElementById('email').value;
-//     if (email == "") {
-//         document.querySelector('.status').innerHTML = "Email cannot be empty";
-//         return false;
-//     } 
-//     var subject =  document.getElementById('subject').value;
-//     if (subject == "") {
-//         document.querySelector('.status').innerHTML = "Subject cannot be empty";
-//         return false;
-//     }
-//     var message =  document.getElementById('message').value;
-//     if (message == "") {
-//         document.querySelector('.status').innerHTML = "Message cannot be empty";
-//         return false;
-//     }
-//     document.querySelector('.status').innerHTML = "Sending...";
-//   }
-
-
-
-
+    });
+});
